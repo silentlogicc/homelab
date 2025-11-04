a82377f8871753ff6bcbb3a5d02e525297a56c2c
silentlogicc <arne.behnfeldt@gmail.com>
2025-11-04 12:50:29 +0100

    add use% output to disk usage script

--- UsefulScripts/disk-usage.sh
+++ UsefulScripts/disk-usage.sh
@@ -7,3 +7,8 @@ df -h /
 #prints timestamp when the script was executed
 
 echo "checked at $(date)"
+
+# reads the 2nd line of df output and extracts the percentage
+
+usage=$(df -h / | awk 'NR==2 {print $5}')
+echo "disk usage is $usage"
