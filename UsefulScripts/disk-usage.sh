cabcfb3547a46169231feac83644527fd541c44d
silentlogicc <arne.behnfeldt@gmail.com>
2025-11-06 00:03:02 +0100

    clean up script and remove unused comments; add "hostname"

--- UsefulScripts/disk-usage.sh
+++ UsefulScripts/disk-usage.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-#shows disk usage in a human readable format
-
-#df -h /
-
 # prints system hostname
-
 echo "hostname: $(hostname)"
 
-#prints timestamp when the script was executed
+# prints system IPv4
+hostname -I | awk '{print $1}'
 
+# prints timestamp when the script was executed
 echo "checked at $(date)"
 
 # reads the 2nd line of df output and extracts the percentage
-
-usage=$(df -h / | awk 'NR==2 {print $5}')
+usage=$(df -hP / | awk 'NR==2 {print $5}')
 echo "disk usage is $usage"
 
 # shows how much free space is left (4th column)
-free=$(df -hP / | awk 'NR==2{print $4}')
+free=$(df -hP / | awk 'NR==2 {print $4}')
 echo "free space is $free"
 
-inodes=$(df -i / | awk 'NR==2{print $4}')
+# shows how many inodes are free
+inodes=$(df -i / | awk 'NR==2 {print $4}')
 echo "free inodes: $inodes"
-
